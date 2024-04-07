5d0e9385450f083eaa304cc91647dcb44dc96e74
omar <ithelpdesk@bitco.co>
2024-04-07 22:08:09 +0300

    [ADD] create the files structure with manafist file

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Managed Service",
+    'summary': " A new app called 'Managed Service' and integrate it with the existing Help Desk App",
+    'description': """
+    """,
+    'author': "Omar Sabouni",
+    'website': "",
+    'category': 'Helpdesk',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    'demo': [
+        #  'demo/demo.xml',
+    ],
+}
+
